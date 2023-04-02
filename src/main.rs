ae6abb0eba3da17120738c1d6d3fd25ab00566c0
solaris5000 <solaris5000tv@gmail.com>
2023-04-02 11:09:26 +0600

    Добавил функцию чтения отпраителя, его почты и пароля приложения из файла, чтобы снова не слить пароль почты в сеть...

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
+
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
@@ -32,6 +35,24 @@ fn index() -> Html<&'static str> {
 
 #[post("/sendmail", data="<user_input>")]
 fn sendmail(user_input : Data) -> String {
+
+    let file = File::open("./creds".to_string()).unwrap(); 
+    
+    let lines = io::BufReader::new(file).lines(); 
+    let mut getsender = true;
+    let mut getsenderemail = true;
+    let mut getpwd = true;
+    //getting NAME <MAILBOX> string
+    let mut sender: String = "".to_owned();
+    let mut senderemail: String = "".to_owned();
+    //getting app password from gmail
+    let mut pwd:String = "".to_owned();
+    for line in lines {
+        if getsender { sender = line.unwrap().clone(); getsender = false; continue; }
+        if getsenderemail { senderemail = line.unwrap().clone(); getsenderemail = false; continue; }
+        if getpwd { pwd = line.unwrap().clone(); getpwd = false; break; }
+    }
+
     let input_data = user_input.open();
 
 
@@ -87,14 +108,14 @@ fn sendmail(user_input : Data) -> String {
 
     let clonetextdbg = text.clone();
     let emmail = Message::builder()
-    .from("NoBody <edu.solaris5000tv@gmail.com>".parse().unwrap())
+    .from(sender.parse().unwrap())
     .to(String::from(email).parse().unwrap())
     .subject("Happy new year")
     .header(ContentType::TEXT_PLAIN)
     .body(String::from(text))
     .unwrap();
 
-    let creds = Credentials::new("solaris5000tv@gmail.com".to_owned(), "teadhemoxwmtzhqd".to_owned());
+    let creds = Credentials::new(senderemail.to_owned(), pwd.to_owned());
 
     let mailer = SmtpTransport::relay("smtp.gmail.com")
     .unwrap()
@@ -103,7 +124,7 @@ fn sendmail(user_input : Data) -> String {
 
     match mailer.send(&emmail) {
         Ok(_) => format!("Message succefully sednded, msg text : {}", clonetextdbg),
-        Err(e) => format!("Message not sended, {}", e),
+        Err(e) => format!("Message not sended, {}\n\nuser {}  email {} pwd {}", e, sender, senderemail, pwd),
     }
 }
 
