f859137d28b6e10854d749f8818cdc095237ec51
solaris5000 <solaris5000tv@gmail.com>
2023-04-10 11:42:07 +0600

    Added static routes, trying to crease responce to get img

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,36 @@
 #[macro_use] extern crate rocket;
 use std::io::Split;
 
-use rocket::response::content::Html;
+use rocket::response::content::Content;
+use rocket::http;
 use rocket::Data;
+use rocket::response::NamedFile;
+use std::env;
+use std::path::{Path, PathBuf};
+
+
 
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
 
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader };
 #[get("/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
 #[get("/")]
+
+fn index() -> Result<NamedFile, std::io::Error> {
+
+    let exepath = env::current_dir().unwrap();
+    let indexpath =  exepath.join("static").join("index.html");
+    println!("{:?}", indexpath);
+    NamedFile::open(indexpath)
+
+/*
 fn index() -> Html<&'static str> {
     Html(r#"<title>GCD Calculator</title>
     <a href='exit'>Terminate this site</a>
@@ -26,12 +43,36 @@ fn index() -> Html<&'static str> {
         <button type='submit'>Send mail</button>
     </form>
     "#
-    )
+    )*/
 }
 
+#[get("/img/<filename>")]
+fn img(filename: String) -> Result<NamedFile, std::io::Error> {
+    let exepath = env::current_dir().unwrap();
+    let imgpath =  exepath.join("img").join(filename);
+    NamedFile::open(imgpath)
+}
 
 #[post("/sendmail", data="<user_input>")]
 fn sendmail(user_input : Data) -> String {
+
+    let file = File::open("./creds".to_string()).unwrap(); 
+    
+    let lines = io::BufReader::new(file).lines(); 
+    let mut getsender = true;
+    let mut getsenderemail = true;
+    let mut getpwd = true;
+    //getting NAME <MAILBOX> string
+    let mut sender: String = "".to_owned();
+    let mut senderemail: String = "".to_owned();
+    //getting app password from gmail
+    let mut pwd:String = "".to_owned();
+    for line in lines {
+        if getsender { sender = line.unwrap().clone(); getsender = false; continue; }
+        if getsenderemail { senderemail = line.unwrap().clone(); getsenderemail = false; continue; }
+        if getpwd { pwd = line.unwrap().clone(); getpwd = false; break; }
+    }
+
     let input_data = user_input.open();
 
 
@@ -87,14 +128,14 @@ fn sendmail(user_input : Data) -> String {
 
     let clonetextdbg = text.clone();
     let emmail = Message::builder()
-    .from("NoBody <edu.solaris5000tv@gmail.com>".parse().unwrap())
+    .from(sender.parse().unwrap())
     .to(String::from(email).parse().unwrap())
     .subject("Happy new year")
     .header(ContentType::TEXT_PLAIN)
     .body(String::from(text))
     .unwrap();
 
-    let creds = Credentials::new("solaris5000tv@gmail.com".to_owned(), "teadhemoxwmtzhqd".to_owned());
+    let creds = Credentials::new(senderemail.to_owned(), pwd.to_owned());
 
     let mailer = SmtpTransport::relay("smtp.gmail.com")
     .unwrap()
@@ -103,7 +144,7 @@ fn sendmail(user_input : Data) -> String {
 
     match mailer.send(&emmail) {
         Ok(_) => format!("Message succefully sednded, msg text : {}", clonetextdbg),
-        Err(e) => format!("Message not sended, {}", e),
+        Err(e) => format!("Message not sended, {}\n\nuser {}  email {} pwd {}", e, sender, senderemail, pwd),
     }
 }
 
@@ -120,6 +161,6 @@ fn main() {
         .extra("template_dir",  "web/templates")
         .unwrap();
 
-    rocket::custom(cfg).mount("/", routes![index, hello, exit, sendmail]).launch();
+    rocket::custom(cfg).mount("/", routes![index, exit, sendmail, img]).launch();
 
 }
